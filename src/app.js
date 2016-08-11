c4f81279d6961317cd5bccbc425ee6be2a1f88e2
David Martin <davidm@potatolondon.com>
2016-08-11 14:21:59 +0100

    fetched data from endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Rx from 'rxjs';
 
 import Geckometer from './components/geckometer.jsx';
 
-ReactDom.render(<Geckometer min={0} max={100} value={30} currency="GBP" />, document.getElementById('app'));
+const endpoint = 'https://widgister.herokuapp.com/challenge/frontend';
+
+Rx.Observable
+  .interval(5000)
+  .switchMap(() => fetch(endpoint).then(res => res.json()))
+  .startWith({min: 0, max: 0, value: 0})
+  .subscribe(data => {
+    console.log(data);
+    ReactDom.render(<Geckometer min={data.min} max={data.max} value={data.value} currency={data.unit} />, document.getElementById('app'));
+  })
